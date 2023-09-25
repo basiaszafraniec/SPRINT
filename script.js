0dabcbe080e6c33d4905c20fd8c3cdbbdf3b52c0
basiaszafraniec <basia.szafraniec@gmail.com>
2023-09-25 09:47:08 +0200

    basket color should work

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ window.onload = () => {
     const basket = document.getElementById("basket");
     basket.addEventListener("mouseover", () => {
         document.getElementById("basketWhite").style.display = "none";
-        document.getElementById("basketYellow").style.display = "block";
+        document.getElementById("basketYellow").style.display = "inline-block";
     })
     basket.addEventListener("mouseout", () => {
-        document.getElementById("basketWhite").style.display = "block";
+        document.getElementById("basketWhite").style.display = "inline-block";
         document.getElementById("basketYellow").style.display = "none";
     })
 
